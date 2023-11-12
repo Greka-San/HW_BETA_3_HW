cdb05121f9254a4878beb64e91dd36038986efd4
GrekaSan <Greka-San@yandex.ru>
2023-11-12 14:10:59 +0300

    fix tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,6 +3,7 @@ from src.widget import hide_the_account_or_card, converts_date
 
 def test_hide_the_account_or_card():
     assert hide_the_account_or_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
+    assert hide_the_account_or_card("Счет 73654108430135874305") == "Счет **4305"
 
 
 def test_converts_date():
